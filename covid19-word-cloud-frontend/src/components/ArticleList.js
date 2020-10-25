473a46191f4b61f9344b9297025cf884fd317909
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2020-10-25 17:41:48 -0400

    distributing components

--- covid19-word-cloud-frontend/src/components/ArticleList.js
+++ covid19-word-cloud-frontend/src/components/ArticleList.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ArticleListItem from './ArticleListItem';
 
 function ArticleList(props) {
+    // Make axios call to the backend
+    var sampleProps1 = {
+        imageLocation: './word-clouds/sample_word_cloud.png',
+        imageAlt: 'A word cloud',
+        title: 'Sample Title 1',
+        content: 'sample content 1 this is a piece of sample content this will contain a paragraph of text that describes the article. It will probably just be a preview of the artile such as the first paragraph or so.'
+    }
+    var sampleProps2 = {
+        imageLocation: './word-clouds/sample_word_cloud.png',
+        imageAlt: 'A word cloud',
+        title: 'Sample Title 2',
+        content: 'sample content 2 this is a piece of sample content this will contain a paragraph of text that describes the article. It will probably just be a preview of the artile such as the first paragraph or so.'
+    }
+    var sampleProps3 = {
+        imageLocation: './word-clouds/sample_word_cloud.png',
+        imageAlt: 'A word cloud',
+        title: 'Sample Title 3',
+        content: 'sample content 3 this is a piece of sample content this will contain a paragraph of text that describes the article. It will probably just be a preview of the artile such as the first paragraph or so.'
+    }
+
     return (
         <div className='ArticleList'>
-            <ArticleListItem></ArticleListItem>
-            <ArticleListItem></ArticleListItem>
-            <ArticleListItem></ArticleListItem>
+            <ArticleListItem imageLocation={sampleProps1.imageLocation} imageAlt={sampleProps1.imageAlt} title={sampleProps1.title} content={sampleProps1.content}></ArticleListItem>
+            <ArticleListItem imageLocation={sampleProps2.imageLocation} imageAlt={sampleProps2.imageAlt} title={sampleProps2.title} content={sampleProps2.content}></ArticleListItem>
+            <ArticleListItem imageLocation={sampleProps3.imageLocation} imageAlt={sampleProps3.imageAlt} title={sampleProps3.title} content={sampleProps3.content}></ArticleListItem>
         </div>
     );
 }
--- covid19-word-cloud-frontend/src/components/ArticleListItem.js
+++ covid19-word-cloud-frontend/src/components/ArticleListItem.js
@@ -4,14 +4,14 @@ function ArticleListItem(props) {
     return (
         <div className='ArticleListItem'>
             <div className='ArticleListItem-Image'>
-                <img src='./word-clouds/sample_word_cloud.png' alt='word cloud of article'></img>
+                <img src={ props.imageLocation } alt={ props.imageAlt }></img>
             </div>
             <div className='ArticleListItem-Article'>
                 <div className='ArticleListItem-Title'>
-                    Sample Article Title
+                    { props.title }
                 </div>
                 <div className='ArticleListItem-Content'>
-                    Est sint dolor fugiat Lorem occaecat ex deserunt sunt duis amet fugiat sint. In dolor mollit anim laboris. Quis est eiusmod eu commodo quis exercitation proident incididunt dolor fugiat. Esse aute veniam do velit consectetur aliquip. Enim mollit consectetur adipisicing aliquip deserunt mollit reprehenderit nisi. Elit incididunt dolor cupidatat deserunt reprehenderit consectetur. Ex tempor adipisicing id dolor nostrud amet sint magna labore sunt laboris...
+                    { props.content }
                 </div>
             </div>
         </div>
